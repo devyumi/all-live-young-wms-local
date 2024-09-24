fa31a54529086f30f281dfedff9c5c518eec1e45
devyumi <yyumiwing@naver.com>
2024-09-24 15:16:40 +0900

    chore: 프로젝트 생성 및 초기 설정

---
+++ src/main/java/com/all_live_young_wms_local/AllLiveYoungWmsLocalApplication.java
@@ -0,0 +1,13 @@
+package com.all_live_young_wms_local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllLiveYoungWmsLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllLiveYoungWmsLocalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/all_live_young_wms_local/AllLiveYoungWmsLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.all_live_young_wms_local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllLiveYoungWmsLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
