e614c026e75f2004d50362d59cf3fde5605884e6
devyumi <yyumiwing@naver.com>
2024-09-29 03:02:26 +0900

    design: bootstrap style 파일 추가

---
+++ src/main/java/com/all_live_young_wms_local/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.all_live_young_wms_local.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper getMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
---
+++ src/main/java/com/all_live_young_wms_local/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.all_live_young_wms_local.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "dashboard";
+    }
+}
